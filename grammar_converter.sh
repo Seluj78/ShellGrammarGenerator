519eb4bbc261591da2ba487941ef588cb09db85f
Guillaume Pouyat <guiforge@gmail.com>
2017-05-29 16:42:26 +0200

    grammar.yacc.example

--- grammar_converter.sh
+++ grammar_converter.sh
@@ -12,7 +12,7 @@ help()
 	echo -e "\t\033[1m-i --input\033[0m  (required), used to specify the grammar file (in yacc format). If not set, will display an error"
 	echo -e "\t\033[1m-o --output\033[0m (optional), if not set, grammar_converter will output into grammar.c and grammar.h files"
 	echo -e "\t\033[1m-h --help\033[0m   (help) Will display help"
-} 
+}
 
 init()
 {
@@ -79,7 +79,7 @@ parse_info()
 
 path_of_file=`dirname $0`
 file_output="$path_of_file/grammar.c"
-file_input="$path_of_file/grammar.yacc"
+file_input="$path_of_file/grammar.yacc.example"
 file_input_tmp="$path_of_file/grammar.yacc.tmp"
 
 if [ $# = 0 ] || [ $# -ge 5 ]; then
