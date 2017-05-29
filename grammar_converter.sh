d5ca157480c5e7dd1cec9ffeda11b87715a02523
Guillaume Pouyat <guiforge@gmail.com>
2017-05-29 16:39:03 +0200

    beaute inside

--- grammar_converter.sh
+++ grammar_converter.sh
@@ -12,7 +12,7 @@ help()
 	echo -e "\t\033[1m-i --input\033[0m  (required), used to specify the grammar file (in yacc format). If not set, will display an error"
 	echo -e "\t\033[1m-o --output\033[0m (optional), if not set, grammar_converter will output into grammar.c and grammar.h files"
 	echo -e "\t\033[1m-h --help\033[0m   (help) Will display help"
-}
+} 
 
 init()
 {
