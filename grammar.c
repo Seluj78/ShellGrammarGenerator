d76d5fbf563625cc40f833bc80fc399f3b66c588
Jules LASNE <jlasne@student.42.fr>
2017-06-01 10:32:34 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ grammar.c
@@ -0,0 +1,75 @@
+#include <parser/parser.h>
+#include <stdint.h>
+
+uint32_t    grammar[][6][4]=
+{
+    [PROGRAM] =
+        {
+        },
+    [COMPLETE_COMMANDS] =
+        {
+        },
+    [COMPLETE_COMMAND] =
+        {
+        },
+    [LIST] =
+        {
+        },
+    [AND_OR] =
+        {
+        },
+    [PIPELINE] =
+        {
+        },
+    [PIPE_SEQUENCE] =
+        {
+        },
+    [COMMAND] =
+        {
+        },
+    [SIMPLE_COMMAND] =
+        {
+        },
+    [CMD_NAME] =
+        {
+        },
+    [CMD_WORD] =
+        {
+        },
+    [CMD_PREFIX] =
+        {
+        },
+    [CMD_SUFFIX] =
+        {
+        },
+    [IO_REDIRECT] =
+        {
+        },
+    [IO_FILE] =
+        {
+        },
+    [FILENAME] =
+        {
+        },
+    [IO_HERE] =
+        {
+        },
+    [HERE_END] =
+        {
+        },
+    [NEWLINE_LIST] =
+        {
+        },
+    [LINEBREAK] =
+        {
+        },
+    [SEPARATOR_OP] =
+        {
+        },
+    [SEPARATOR] =
+        {
+        },
+    [SEQUENTIAL_SEP] =
+        {
+        }
+};
